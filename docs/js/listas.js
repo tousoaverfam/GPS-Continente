63693679c471ac9c9240b525f0771f45394b616a
tousoaverfam <apenasaobservar@gmail.com>
2026-02-13 16:36:13 +0000

    Update listas.js

--- docs/js/listas.js
+++ docs/js/listas.js
@@ -1,8 +1,14 @@
 const listas = JSON.parse(localStorage.getItem('listas')) || [];
 const ul = document.getElementById('listasGuardadas');
 
-listas.forEach(lista => {
+listas.forEach((lista, index) => {
   const li = document.createElement('li');
   li.textContent = lista.nome;
+  li.style.animationDelay = index * 0.1 + "s";
   ul.appendChild(li);
 });
+
+function eliminarTodas() {
+  localStorage.removeItem('listas');
+  location.reload();
+}
